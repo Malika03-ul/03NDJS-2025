ee7eaab528a2065bb4b99d7c41ed994c39b5c1a1
malika <ndjonguetiadeumalikalucresse@gmail.com>
2025-04-14 17:50:22 +0200

    importer un module pour effectuer et afficher les operations

---
+++ DAY1/sandbox/first_step/main.js
@@ -0,0 +1,11 @@
+const math = require(`./math.js`);
+
+// Example usage
+const sum = math.sum(5, 7);
+console.log(`The sum is :${sum}`);
+const diff = math.diff(10,4);
+console.log(`The difference is: ${diff}`);
+const prod = math.prod(3, 6);
+console.log(`The product is :${prod}`);
+const quot = math.quot(8, 2);
+console.log(`The quotient is: ${quot}`);
