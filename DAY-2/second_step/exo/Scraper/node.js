8605ff3097ea9780344a2561d9dbc500af9618b7
malika <ndjonguetiadeumalikalucresse@gmail.com>
2025-04-15 16:14:59 +0200

    permettre a cheerio d'acceder aux données de mon tableau via son url

---
+++ DAY-2/second_step/exo/Scraper/node.js
@@ -0,0 +1,19 @@
+const cheerio = require('cheerio'); // importation du module cheerio
+const url = 'https://fr.wikipedia.org/wiki/Liste_des_communes_de_Belgique_par_population'
+
+// fonction générale qui cherche et recupère les données de mon tableau
+
+function initScraped() {
+  console.log("nouveau scraping de mon tableau");
+};
+
+ // message de bienvenue daans mon scraped
+
+import * as cheerio from 'cheerio';
+
+const $ = await cheerio.fromURL('https://fr.wikipedia.org/wiki/Liste_des_communes_de_Belgique_par_population');
+
+
+
+
+
