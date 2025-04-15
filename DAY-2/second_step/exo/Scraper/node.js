5a91c02695fe8fbb56364d360614188a8f8b9113
malika <ndjonguetiadeumalikalucresse@gmail.com>
2025-04-15 20:41:11 +0200

    revision de mon code

--- DAY-2/second_step/exo/Scraper/node.js
+++ DAY-2/second_step/exo/Scraper/node.js
@@ -1,45 +1,35 @@
-const cheerio = require('cheerio'); // importation du module cheerio
+const cheerio = require('cheerio');
+const fs = require('fs');
+
 const url = 'https://fr.wikipedia.org/wiki/Liste_des_communes_de_Belgique_par_population'
 
-// fonction générale qui cherche et recupère les données de mon tableau
+async function scrapeWikipedia() {
 
-const $ = await cheerio.fromURL('https://fr.wikipedia.org/wiki/Liste_des_communes_de_Belgique_par_population'); // tant que les élements de mon url ne sont pas tous parcouru il n'opère aucun choix
+	const $ = await cheerio.fromURL('https://fr.wikipedia.org/wiki/Liste_des_communes_de_Belgique_par_population');
 
-// importe les modules nécessaires
+	const communes = [];
 
-const fs = require('fs'); // pour lire le fichier local
+	const table = $('.wikitable.sortable').first();
 
-// crée un tableau vide pour stocker les communes extraites
-const communes = [];
+	table.find('tbody tr').each((i, row) => {
 
-// sélectionne le 1er tableau ayant la classe "wikitable sortable": C’est élement du tableau qui contient le classement des communes
-const table = $('.wikitable.sortable').first();
+		const cells = $(row).find('td');
 
-// Pour chaque ligne du tableau :
-table.find('tbody tr').each((i, row) => {
-  // récupère toutes les cellules <td> de cette ligne
-  const cells = $(row).find('td');
+  		if (cell.length) return;
 
-  //  la ligne n’a pas au moins 3 cellules, on l’ignore (souvent l’entête ou ligne vide)
-  if (cells.length < 3) return;
+		const commune = {
 
-  // construit un objet représentant une commune
-  const commune = {
-    rank: $(cells[0]).text().trim(),        // Colonne 1 : Rang
-    name: $(cells[1]).text().trim(),        // Colonne 2 : Nom de la commune
-    population: $(cells[2]).text().trim()   // Colonne 3 : Population
-  };
+    			 $(cell).text().trim(),
+  		};
 
-  // ajoute l’objet au tableau final
-  communes.push(commune);
-});
+  		communes.push(commune);
+	});
 
-// affiche les 10 premières communes dans un tableau dans le terminal
-console.table(communes.slice(0, 100));
+	console.table(communes.slice(0, 100));
 
-// Optionnel : afficher combien de communes ont été extraites
-console.log(`\n ${communes.length} communes extraites avec succès.`);
-
+	console.log(`\n ${communes.length} communes extraites avec succès.`);
+}
+scrapeWikipedia();
 
 
 
